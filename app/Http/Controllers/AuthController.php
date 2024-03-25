f31a5af87653e6c68a0c8550f1cdeb25f36da302
Mehdi <elmahdi20erraji@gmail.com>
2024-03-25 11:35:33 +0000

    auth done

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UserRegistration;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+
+    public function user()
+    {
+        return 'users authentication';
+    }
+    public function register(UserRegistration $request)
+    {
+        $data = $request->validated();
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+
+        return response()->json(['message' => 'User registered successfully', 'user' => $user],201);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+    
+        if (!Auth::attempt($credentials)) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+    
+        $user = Auth::user();
+        $token = $user->createToken('API Token')->plainTextToken;
+    
+        return response()->json(['user' => $user, 'token' => $token], 200);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'Logged out successfully'], 200);
+    }
+}
