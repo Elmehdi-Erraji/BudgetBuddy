535b7a730848d09227ea216f0b8a5ef911c70863
Mehdi <elmahdi20erraji@gmail.com>
2024-03-25 14:07:56 +0000

    small fix

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
+//commands 
+/**
+ * php artisan l5-swagger:generate                              ==> to generate api documentation
+ * 
+ *  php artisan test                                            ==> run all the tests
+ * 
+ *  php artisan test tests/Feature/DepenseControllerTest.php    ==>run the depense controller tests 
+ */
